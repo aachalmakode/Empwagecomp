be2d1e90bc5b3d66906a9478a3fd6ecd50e45fc6
aachalmakode <aachalmakode900@gmail.com>
2021-05-22 19:17:57 -0400

    checking emp is present or absent

--- empwage.sh
+++ empwage.sh
@@ -1,2 +1,12 @@
+
 #!/bin/bash -x
-echo "welcome to emp wage cpmputation"
+echo "welcome to emp wage computation"
+
+isPresent=1
+randomCheck=$((RANDOM%2))
+if [ $isPresent -eq $randomCheck ]
+then
+         echo "employee is present"
+else 
+         echo "employee is absent"
+fi
