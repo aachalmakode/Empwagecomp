1ee536ef600e53a1c1bcd0d204f9d46786bea6fc
aachalmakode <aachalmakode900@gmail.com>
2021-05-23 10:51:49 -0400

    part time wages

--- empwage.sh
+++ empwage.sh
@@ -20,3 +20,33 @@ wagePerDay=$(( 20 * 8))
 echo "daily employee wages = $wagePerDay"
 
 
+echo "calculate Part time Employee & wage"
+
+isFullTimeEmp=1;
+isPartTimeEmp=2;
+
+wagePerHour=20;
+salary=0;
+totalSalary=0;
+
+empName=emp
+
+echo "calculate wages"
+
+read -p "enter the number of working days = " numDays
+
+for (( days=1; days<=$numDays; days++ ));
+do
+	employeeCheck=$(( 1 + RANDOM % 2 ));
+	case $employeeCheck in
+		$isFullTimeEmp)
+			empName=FullTimeEmployee
+			empHrs=8;;
+		$isPartTimeEmp)
+			empName=PartTimeEmployee
+			empHrs=4;;
+	esac
+salary=$(( $empHrs * $wagePerHour ))
+	echo "salary of $empName on the day $days is $salary"
+	totalSalary=$(( $totalSalary + $salary ))
+done
